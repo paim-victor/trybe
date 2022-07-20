d2ede24409467dc78a3caf07e83f4b62bd02c19f
Paim <victor-paim@hotmail.com>
2022-07-20 15:42:06 -0300

    praticando js

---
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-introducao-js/scripts.js
@@ -0,0 +1,12 @@
+const myName = "Victor Paim";
+const birthCity = "Salvador-BA"
+let birthYear = 1990;
+
+birthYear = 2030;
+birthCity = "Petropolis-RJ"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/* A msg de erro ocorre por que o birthCity é uma constante e não podemos troca-la, diferentemente de uma variavel, que pode ser modificada. */
