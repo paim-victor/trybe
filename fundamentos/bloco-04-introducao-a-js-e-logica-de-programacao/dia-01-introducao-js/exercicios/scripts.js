e02e45b4b6b0a4234611598ba5e8d22807214911
Paim <victor-paim@hotmail.com>
2022-07-20 18:20:45 -0300

    fiz metade do ultimo exercicio do dia hoje

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-introducao-js/exercicios/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-introducao-js/exercicios/scripts.js
@@ -75,4 +75,92 @@
 //   console.log("ERR0R!!!")
 // }
 
+// let nota = 3.75;
+
+// if (nota >= 9) {
+//   console.log("A")
+// } 
+// else if(nota >= 8) {
+//   console.log("B")
+// } 
+// else if(nota >= 7) {
+//   console.log("C")
+// }
+// else if(nota >= 6) {
+//   console.log("D")
+// }
+// else if(nota >= 5) {
+//   console.log("E")
+// }
+// else if(nota < 5) {
+//   console.log("F")
+// } 
+// else {
+
+// }
+
+// const numero1 = 10;
+// const numero2 = 11;
+// const numero3 = 13;
+
+// if (numero1 % 2 === 0|| numero2 % 2 === 0 || numero3 % 2 === 0) {
+//   console.log("true")
+// } 
+// else {
+//   console.log("false")
+// }
+
+// const numero1 = 10;
+// const numero2 = 12;
+// const numero3 = 8;
+
+// if (numero1 % 2 === 1 || numero2 % 2 === 1 || numero3 % 2 === 1) {
+//   console.log("true")
+// }
+// else {
+//   console.log("false")
+// }
+
+// const custoProduto = 20; 
+// const valorDeVenda = 15;
+// const imposto = 0.2;
+// let lucro = 0;
+
+// lucro = (custoProduto * imposto) - valorDeVenda 
+
+// if(lucro > 0) {
+//   console.log(lucro)
+// }
+// else {
+//   console.log("ERROR!!!")
+// }
+
+let salarioBruto = 5200.00;
+let salarioLiquido = 0;
+let inss = 0;
+let ir = 0;
+
+if(salarioBruto <= 1556.94) {
+  inss = salarioBruto * 0.08
+  salarioLiquido = salarioBruto - inss 
+  console.log(salarioLiquido)
+} 
+else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92) {
+  inss = salarioBruto * 0.09
+  salarioLiquido = salarioBruto - inss
+  console.log(salarioLiquido)
+}
+else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82) {
+  inss = salarioBruto * 0.11
+  salarioLiquido = salarioBruto - inss
+  console.log(salarioLiquido)
+}
+else if (salarioBruto > 5189.82) {
+  inss = 570.88
+  salarioLiquido = salarioBruto - inss
+  console.log(salarioLiquido)
+}
+else {
+
+}
 
