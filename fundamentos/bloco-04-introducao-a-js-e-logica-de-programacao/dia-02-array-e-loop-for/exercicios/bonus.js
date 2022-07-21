ba28598fbc6d6f63e85108d3889fab25fa4fbcb8
Paim <victor-paim@hotmail.com>
2022-07-21 19:21:51 -0300

    terminei os bonus

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/bonus.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/bonus.js
@@ -1,6 +1,4 @@
 
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //bonus1 (uma forma)
 // numbers.sort(function(a,b) {
 //   if(a > b) return 1;
@@ -22,3 +20,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //bonus3
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let array = [];
+
+for (let index = 0; index < numbers.length; index += 1){
+  if(index + 1 < numbers.length){
+    array.push(numbers[index] * numbers[index + 1]);
+  } else {
+    array.push(numbers[index] * 2)
+  }
+}
+
+console.log(array)
