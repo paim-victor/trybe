61912f68b21ec43d9b198e00d7431afc5ad37fe2
Paim <victor-paim@hotmail.com>
2022-07-20 21:28:00 -0300

    batendo cabeça com arrays

---
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-array-e-loop-for/arrays/scripts.js
@@ -0,0 +1,27 @@
+// let pizzas = ['4 Queijos', 'Franco com Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+
+// //pizzas[5] = 'Peito de Peru'
+
+// pizzas.push('Peito de Peru')
+// pizzas.push('Gratinada')
+
+// //console.log('Menu de sabores: ' + pizzas);
+// //console.log(pizzas.length)
+// //console.log(pizzas.sort());
+// //console.log(pizzas[3])
+
+// for (let index = 0; index < pizzas.length; index++) {
+//   console.log(pizzas[index])
+// }
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = 0;
+
+// console.log(menu[1]);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = 0;
+
+for (let indexOfPortfolio = 0;  indexOfPortfolio < menu.length; indexOfPortfolio++){
+  console.log(menu[indexOfPortfolio]);
+}
---
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/scripts.js
@@ -0,0 +1,50 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1. console.log(numbers)
+
+//2. const reducer = (accumulator, curr) => accumulator + curr;
+// console.log(numbers.reduce(reducer));
+
+//3. function ArrayAvg(numbers) {
+//   var i = 0, summ = 0, ArrayLen = numbers.length;
+//   while (i < ArrayLen) {
+//       summ = summ + numbers[i++];
+// }
+//   return summ / ArrayLen;
+// }
+// let a = ArrayAvg(numbers);
+// console.log(a)
+
+//4. function ArrayAvg(numbers) {
+//     var i = 0, summ = 0, ArrayLen = numbers.length;
+//     while (i < ArrayLen) {
+//         summ = summ + numbers[i++];
+//   }
+//     return summ / ArrayLen;
+//   }
+//   let a = ArrayAvg(numbers);
+//   console.log(a)
+
+//   if (a > 20){
+//     console.log('valor maior que 20')
+//   } else{
+//     console.log('valormenor ou igual a 20.')
+//   }
+
+//5. let maiorNumero = Math.max.apply(null, numbers);
+
+// console.log(maiorNumero)
+
+
+
+
+let pares = []
+let impares = []
+
+for (i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(pares[i])
+  } else {
+    console.log(impares[i])
+  }
+}
