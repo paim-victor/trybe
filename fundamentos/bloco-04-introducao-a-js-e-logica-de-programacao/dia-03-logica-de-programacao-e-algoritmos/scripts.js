755f1ba325dae505df8249a864a700eb1c5d7c6a
Paim <victor-paim@hotmail.com>
2022-07-22 16:26:06 -0300

    estudando logica de programação e algoritmos

---
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,17 @@
+// const n = 9;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+
+// const n = 5;
+
+// let resultado = 0;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado += index
+// };
+// console.log(resultado)
+
+// let 
