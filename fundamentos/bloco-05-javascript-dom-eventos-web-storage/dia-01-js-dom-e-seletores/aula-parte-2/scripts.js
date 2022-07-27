ac48b91884cf132c5b38edfe8b211a3d0114df70
Paim <victor-paim@hotmail.com>
2022-07-27 20:01:50 -0300

    finalizando o dia

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-js-dom-e-seletores/aula-parte-2/scripts.js
@@ -0,0 +1,7 @@
+// let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
+
+// for (let i = 0; i < pilotosDeF1.length; i += 1) {
+//   (pilotosDeF1[i]).innerText = "Lewis Hamilton"
+// }
+
+// document.getElementsByTagName("span")[0].innerText = "Carlos Sainz"
