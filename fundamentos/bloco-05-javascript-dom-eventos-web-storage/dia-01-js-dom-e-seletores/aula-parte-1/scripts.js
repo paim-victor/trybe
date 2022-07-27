ca63ef08be9b68904702974fcea16528493d83a1
Paim <victor-paim@hotmail.com>
2022-07-27 14:47:29 -0300

    iniciando DOM

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-js-dom-e-seletores/aula-parte-1/scripts.js
@@ -0,0 +1,6 @@
+// console.log(document.getElementById('teste').innerText);
+// console.log(document.getElementById('teste').innerHTML);
+// console.log(document.getElementById('teste').style);
+
+// document.getElementById('teste').innerText = "Aprendizados da aula de hoje!"
+console.log(document.getElementById('teste').innerText = "Aprendizados da aula de hoje!")
